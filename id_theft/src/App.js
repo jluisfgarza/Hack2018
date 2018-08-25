2f6678b3d74617a0979eb6c1f7f238a532160f14
jluisfgarza <jluisfgarza@gmail.com>
2018-08-25 15:19:32 -0500

    facebook login

--- id_theft/src/App.js
+++ id_theft/src/App.js
@@ -1,15 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { provider, auth } from "./firebase/client";
+import "./App.css";
+import { func } from "prop-types";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      token: "",
+
+    };
+  }
+
+  componentWillMount() {
+    auth().onAuthStateChanged(function(user, token) {
+      if (user && token) {
+        this.setState({
+          user: this.user,
+          token: this.token
+        });
+      }
+    });
+  }
+
+  login = () => {
+    auth()
+      .signInWithPopup(provider)
+      .then(({ user, token }) => {
+        this.setState({
+          user: this.user,
+          token: this.token
+        });
+      }).catch(function(error){
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+      });
+  };
+
+  logout = () => {
+    auth()
+      .signOut()
+      .then(() => {
+        this.setState({
+          user: null,
+          token: ""
+        });
+      });
+  };
+
   render() {
+    const { user } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Welcome to ID Theft</h1>
-        </header>
+        <p> {user ? `Hi, ${user.displayName}!` : "Hi!"} </p>{" "}
+        <button onClick={this.login}>Login with Facebook </button>
+        <button onClick={this.logout}>Logout </button>{" "}
       </div>
     );
   }
